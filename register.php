9b3576a7b37c50477855a342efd65e85422fecb8
MaryamTaraif <116751029+MaryamTaraif@users.noreply.github.com>
2023-05-02 00:24:41 +0300

    Update register.php

--- register.php
+++ register.php
@@ -1,44 +1,44 @@
-<?php include 'header.html' ?>
+<?php include 'header.html'?>
 
 <section class="login first grey">
-    <div class="container">
-        <div class="box-wrapper">				
-            <div class="box box-border">
-                <div class="box-body">
-                    <h4>Register</h4>
-                    <form>
-                        <div class="form-group">
-                            <label>Username</label>
-                            <input type="text" name="name" class="form-control">
-                        </div>
+			<div class="container">
+				<div class="box-wrapper">				
+					<div class="box box-border">
+						<div class="box-body">
+							<h4>Register</h4>
+							<form>
+								<div class="form-group">
+									<label>Username</label>
+									<input type="text" name="name" class="form-control">
+								</div>
 
-                        <div class="form-group">
-                            <label class="fw">Password</label>
-                            <input type="password" name="password" class="form-control">
-                        </div>
-
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" name="email" class="form-control">
-                        </div>
-
-
-                        <div class="form-group text-right">
-                            <button class="btn btn-primary btn-block">Register</button>
-                        </div>
-                        <div class="form-group text-center">
-                            <span class="text-muted">Already have an account?</span> <a href="login.php">Login</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
+								<div class="form-group">
+									<label class="fw">Password</label>
+									<input type="password" name="password" class="form-control">
+								</div>
+                                            
+								<div class="form-group">
+									<label>Email</label>
+									<input type="email" name="email" class="form-control">
+								</div>
+								
+						
+								<div class="form-group text-right">
+									<button class="btn btn-primary btn-block">Register</button>
+								</div>
+								<div class="form-group text-center">
+									<span class="text-muted">Already have an account?</span> <a href="login.php">Login</a>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 
 
 
 
-<?php include 'footer.html' ?>
+
+
+<?php include 'footer.html'?>
