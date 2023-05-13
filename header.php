5494120a420c3c76b921854f2e5d9c5622d5330a
MaryamTaraif <116751029+MaryamTaraif@users.noreply.github.com>
2023-05-13 18:05:28 +0300

    logout added

--- header.php
+++ header.php
@@ -30,6 +30,8 @@
         <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/skins/all.css">
         <link rel="stylesheet" href="css/demo.css">
+
+
     </head>
 
     <body>
@@ -115,7 +117,27 @@
                                     echo '</a></li>';
                                  }
                             }
+                            
+                            
                             ?>
+                            
+                            <?php
+                            if(!empty($_SESSION['user_id'])){
+                             echo'  <li class="dropdown magz-dropdown"><a href="#">My Account <i class="ion-ios-arrow-right"></i></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#"><i class="icon ion-person"></i> Dashbaord</a></li>
+    
+                                    <li class="divider"></li>
+                                    <li><a href="logout.php"><i class="icon ion-log-out"></i> Logout</a></li>
+                                </ul>
+                            </li>';
+                                       
+                            }
+                            
+                            
+                            
+                            ?>
+                              
                         </ul>
                     </div>
                 </div>
