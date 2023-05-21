e113e49047cc9f03e0919dd813a1405779e59ff5
KawtharAlakri <202003447@student.polytechnic.bh>
2023-05-22 01:07:51 +0300

    improve feedback and UI

--- myArticles.php
+++ myArticles.php
@@ -36,7 +36,10 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Author') {
                     if (this.responseText === "published") {
                         location.reload();
                         alert("Published Successfully");
-                    } 
+                    }
+                    else {
+                        alert(this.responseText);
+                    }
                 }
             };
             xhttp.open("GET", "publishArticle.php?p_id=" + articleId, true);
@@ -177,17 +180,17 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Author') {
                         <div class="aside-body">
                             <div class="featured-author">
                                 <div class="featured-author-inner">
-                                    <div class="featured-author-cover" style="background-image: url('images/news/img15.jpg');">
+                                    <div class="featured-author-cover" style="background-image: url('images/back2.jpg');">
                                         <div class="badges">
-                                            <div class="badge-item"><i class="ion-star"></i> Featured</div>
+                                            <div class="badge-item"><i class="ion-star"></i> PROFILE</div>
                                         </div>
                                         <div class="featured-author-center">
                                             <figure class="featured-author-picture">
-                                                <img src="images/img01.jpg" alt="Sample Article">
+                                                <img src="images/profilePic.png">
                                             </figure>
                                             <div class="featured-author-info">
-                                                <h2 class="name">John Doe</h2>
-                                                <div class="desc">@JohnDoe</div>
+                                                <h2 class="name"><?php echo $_SESSION['username']?></h2>
+                                                <div class="desc"><?php echo $_SESSION['role']?></div>
                                             </div>
                                         </div>
                                     </div>
@@ -195,47 +198,28 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Author') {
                                         <div class="featured-author-count">
                                             <div class="item">
                                                 <a href="#">
-                                                    <div class="name">Posts</div>
+                                                    <div class="name">Articles</div>
                                                     <div class="value">208</div>														
                                                 </a>
                                             </div>
                                             <div class="item">
                                                 <a href="#">
-                                                    <div class="name">Stars</div>
+                                                    <div class="name">Views</div>
                                                     <div class="value">3,729</div>														
                                                 </a>
                                             </div>
                                             <div class="item">
                                                 <a href="#">
-                                                    <div class="icon">
-                                                        <div>More</div>
-                                                        <i class="ion-chevron-right"></i>
-                                                    </div>														
+                                                    <div class="name">Likes</div>
+                                                    <div class="value">3,729</div>														
                                                 </a>
                                             </div>
                                         </div>
                                         <div class="featured-author-quote">
-                                            "Eur costrict mobsa undivani krusvuw blos andugus pu aklosah"
+                                            "Writing, to me, is simply thinking through my fingers."
                                         </div>
-                                        <div class="block">
-                                            <h2 class="block-title">Photos</h2>
-                                            <div class="block-body">
-                                                <ul class="item-list-round" data-magnific="gallery">
-                                                    <li><a href="images/news/img06.jpg" style="background-image: url('images/news/img06.jpg');"></a></li>
-                                                    <li><a href="images/news/img07.jpg" style="background-image: url('images/news/img07.jpg');"></a></li>
-                                                    <li><a href="images/news/img08.jpg" style="background-image: url('images/news/img08.jpg');"></a></li>
-                                                    <li><a href="images/news/img09.jpg" style="background-image: url('images/news/img09.jpg');"></a></li>
-                                                    <li><a href="images/news/img10.jpg" style="background-image: url('images/news/img10.jpg');"></a></li>
-                                                    <li><a href="images/news/img11.jpg" style="background-image: url('images/news/img11.jpg');"></a></li>
-                                                    <li><a href="images/news/img12.jpg" style="background-image: url('images/news/img12.jpg');"><div class="more">+2</div></a></li>
-                                                    <li class="hidden"><a href="images/news/img13.jpg" style="background-image: url('images/news/img13.jpg');"></a></li>
-                                                    <li class="hidden"><a href="images/news/img14.jpg" style="background-image: url('images/news/img14.jpg');"></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="featured-author-footer">
-                                            <a href="#">See All Authors</a>
-                                        </div>
+                                        
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -247,7 +231,7 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'Author') {
             $tops = Article::getAuthorTops($_SESSION['user_id']);
             if (!empty($tops)){
                 echo '<aside>
-                        <h1 class="aside-title">Your Top Aticles </h1> <div class="aside-body">';
+                        <h1 class="aside-title">Your Top Articles </h1> <div class="aside-body">';
                 for ($i = 0; $i < count($tops); $i++) {
                     echo '<article class="article-mini">
                                             <div class="inner">
--- publishArticle.php
+++ publishArticle.php
@@ -3,9 +3,23 @@ include 'debugging.php';
 if (!isset($_GET['p_id']) || empty($_GET['p_id'])) {
     exit;
 }
-
-$publish = Article::publish($_GET['p_id']);
-if ($publish){
-    echo 'published';
+//validate that it has media of type photo and either video aor audio before publishing 
+$valid = true;
+if (empty(Media::getPhotoURL($_GET['p_id']))) {
+    $valid = false;
 }
+if (empty(Media::getVideoURL($_GET['p_id'])) || empty(Media::getAudioURL($article->getArticle_id()))) {
+    $valid = false;
+}
+
+if ($valid){
+    $publish = Article::publish($_GET['p_id']);
+    if ($publish){
+        echo 'published';
+    }
+}
+else {
+    echo 'Sorry, this article is still incomplete.';
+}
+    
 ?>
