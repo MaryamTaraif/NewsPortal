9f5cc18bdba31a0cbb87b6ccff1835316190a75e
OmarAlshams <omar_os@icloud.com>
2023-05-22 11:29:40 +0300

    updated Users class
    Updated the class and the edit, delete and view files

--- edit_User.php
+++ edit_User.php
@@ -1,28 +1,95 @@
 <?php
-  // Update the user details in the database
-    $update_query = "UPDATE dbProj_User SET username='$username', email='$email', password='$password' WHERE id='$user_id'";
-    mysqli_query($this->dblink, $update_query);
 
-    // Redirect back to the admin panel
-    header('Location: admin.php');
 
-// Get the user ID from the URL parameter
-$user_id = $_GET['id'];
+include 'debugging.php';
 
-// Get the user details from the database
-$user_query = "SELECT * FROM dbProj_User WHERE id='$user_id'";
-$user_result= mysqli_query($this->dblink, $user_query);
-$user = mysqli_fetch_assoc($user_result);
+$page_title = 'Edit User';
 
-// Display the form to edit the user details
-echo "<h2>Edit User</h2>";
-echo "<form method='post' action='edit_User.php'>";
-echo "<input type='hidden' name='id' value='".$user['user_id']."'>";
-echo "<label for='username'>Username:</label>";
-echo "<input type='text' id='username' name='username' value='".$user['username']."'><br>";
-echo "<label for='email'>Email:</label>";
-echo "<input type='email' id='email' name='email' value='".$user['email']."'><br>";
-echo "<label for='password'>Password:</label>";
-echo "<input type='password' id='password' name='password'><br>";
-echo "<input type='submit' name='submit' value='Save'>";
-echo "</form>";
+include 'header.php';
+
+$id = 0;
+
+//the id of the user is first passed to the form as a parameter so we retrieve it from the $_GET global array
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
+//after the page is first submitted the id is stored in a hidden field on the page so we get it frm the $_POST array
+elseif (isset($_POST['id'])) {
+    $id = $_POST['id'];
+} else {
+    //no id parameter is present so we do not go any further
+    echo '<p class="error">No User id Parameter</p>';
+
+    include 'footer.html';
+
+    exit();
+}
+
+$user = new Users();
+$user->initWithUid($id);
+
+//perfrom the following if the user has submitted the form 
+if (isset($_POST['submitted'])) {
+
+    $oldName = $user->getUsername();
+
+//populate the user object member variables from values on the form
+    $user->setUsername($_POST['Username']);
+    $user->setEmail($_POST['Email']);
+    $user->setPassword($_POST['Password']);
+    $user->setType_name($_POST['Type_name']);
+
+
+    if (!$user->initWithUsername()) {// username exists
+        
+        echo "<h2> Thankyou </h2><p>".$user->getUsername()." Exists</p>";
+        $user->setUsername($oldName);
+        
+    }else {
+
+        $errors = $user->isValid();
+
+        if (empty($errors)) {
+            //update the user 
+            $user->updateDB();
+            echo "<h2> Thankyou </h2><p>.$user->getUsername(). is updated</p>";
+        } else {
+            echo '<div style="width:50%; background:#FFFFFF; margin:0 auto;">
+                <p class="error"> The following errors occurred: <br/>';
+            foreach ($errors as $err) {
+                echo "$err <br/>";
+            }
+            echo '</p></div>';
+        }
+    }
+} // end if submitted conditional
+
+echo '<h1>Edit User</h1>';
+
+
+//create a new user data object and populate it using the get() method
+//this will show the form with the fields already populated with values from the $user object created above 
+//see the CSS file to see what effect the id="stylized" properties have on the form 
+echo '<div id="stylized" class="myform"> 
+         <form action="edit_User.php" method="post">
+        <br />
+        <h3>Edit User: ' . $user->getUsername() . '</h3>
+        <br />
+           <label>Username</label>    <input type="text" name="Username" value="' . $user->getUsername() . '" />
+           <label>Email Address</label> <input type="text" name="Email" value="' . $user->getEmail() . '"/>
+           <label>Password</label>      <input type="password" name="Password" value="' . $user->getPassword() . '"/>
+           <label>Type_name</label> <input type="text" name="Type_name" value="' . $user->setType_name() . '"/>
+           <input type="submit" class ="DB4Button" name="submit" value="update" />
+        
+         <input type ="hidden" name="submitted" value="TRUE">
+         <input type ="hidden" name="id" value="' . $id . '"/>
+         </form>
+        <div class="spacer"></div>
+        </div>';
+
+
+
+
+
+include 'footer.html';
+?>
---
+++ view_Users.php
@@ -0,0 +1,53 @@
+<?php
+
+
+include 'debugging.php';
+
+include 'header..php';
+
+echo '<h1> Users </h1>';
+
+
+$users = new Users();
+$row = $users->getAllusers();
+
+if (!empty($row)) {
+    echo '<br />';
+    //display a table of results
+    echo '<table align="center" cellspacing = "2" cellpadding = "4" width="75%">';
+    echo '<tr bgcolor="#87CEEB">
+          <td><b>Edit</b></td>
+          <td><b>Delete</b></td>
+          <td><b><a href="view_Users.php">username</a></b></td>
+          <td><b><a href="view_Users.php">email</a></b></td>
+          <td><b><a href="view_Users.php">type_name</a></b></td></tr>';
+
+
+//above is the header
+//loop below adds the User details    
+    //use the following to set alternate backgrounds 
+    $bg = '#eeeeee';
+
+    for ($i = 0; $i < count($row); $i++) {
+        $bg = ($bg == '#eeeeee' ? '#ffffff' : '#eeeeee');
+
+        echo '<tr bgcolor="' . $bg . '">
+            <td><a href="edit_User.php?id=' . $row[$i]->uid . '">Edit</a></td>
+            <td><a href="delete_User.php?id=' . $row[$i]->uid . '">Delete</a></td>
+                <td>' . $row[$i]->user_id . '</td>
+            <td>' . $row[$i]->username . '</td>
+                <td>' . $row[$i]->password . '</td>
+            <td>' . $row[$i]->email . '</td>
+                <td>' . $row[$i]->type_name . '</td>
+              </tr>';
+    }
+    echo '</table>';
+} else {
+    echo '<p class="error">' . $q . '</p>';
+    echo '<p class="error"> Oh dear. There was an error</p>';
+    echo '<p class="error">' . mysqli_error($dbc) . '</p>';
+}
+
+
+include 'footer.html';
+?>
