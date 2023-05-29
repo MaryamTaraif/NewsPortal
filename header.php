4a4808a75bd747c0307592a9eb064868ceaf70ef
KawtharAlakri <202003447@student.polytechnic.bh>
2023-05-29 21:32:36 +0300

    responsive header

--- header.php
+++ header.php
@@ -33,11 +33,25 @@
     <link rel="stylesheet" href="scripts/ionicons/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
 
+    
+<!-- JS -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.migrate.js"></script>
+<script src="scripts/bootstrap/bootstrap.min.js"></script>
+<script>var $target_end = $(".best-of-the-week");</script>
+<script src="scripts/jquery-number/jquery.number.min.js"></script>
+<script src="scripts/owlcarousel/dist/owl.carousel.min.js"></script>
+<script src="scripts/magnific-popup/dist/jquery.magnific-popup.min.js"></script>
+<script src="scripts/easescroll/jquery.easeScroll.js"></script>
+<script src="scripts/sweetalert/dist/sweetalert.min.js"></script>
+<script src="scripts/toast/jquery.toast.min.js"></script>
+<script src="js/demo.js"></script>
+<script src="js/e-magz.js"></script>
 
 </head>
 
 <body>
-    <header class="primary">
+    <header class="primary fixed">
         <div class="firstbar">
             <div class="container">
                 <div class="row">
